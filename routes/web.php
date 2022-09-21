8d949d735ba54e46226c47f5b3d2493d8fe6fec3
abdoulDaff <abdoulniallydaff@gmail.com>
2022-09-21 23:39:43 +0000

    Create Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -27,5 +28,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'show'])
+    ->middleware('auth', 'verified');
 
 require __DIR__.'/auth.php';

f5eecbf5e7ddae4fe5249a065075984eefa67054
abdoulDaff <abdoulniallydaff@gmail.com>
2022-09-22 03:53:50 +0000

    update the destroy method on our ChirpController class

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -104,5 +104,10 @@ class ChirpController extends Controller
     public function destroy(Chirp $chirp)
     {
         //
+        $this->authorize('delete', $chirp);
+ 
+        $chirp->delete();
+ 
+        return redirect(route('chirps.index'));
     }
 }

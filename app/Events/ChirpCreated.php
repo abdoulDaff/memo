8935102311ac60b11ea44f88b315bc17e09baac6
abdoulDaff <abdoulniallydaff@gmail.com>
2022-09-22 04:14:37 +0000

    Update event

--- app/Events/ChirpCreated.php
+++ app/Events/ChirpCreated.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\Chirp;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -19,7 +20,7 @@ class ChirpCreated
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(public Chirp $chirp)
     {
         //
     }

fd1b6a8e31bf8eaadd91674d5bae6d4a709b4e0c
abdoulDaff <abdoulniallydaff@gmail.com>
2022-09-22 04:16:44 +0000

    Dispatching the event

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,7 +13,9 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
-
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
     public function user()
     {
         return $this->belongsTo(User::class);

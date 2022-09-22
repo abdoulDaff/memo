30fddf07dafc591a716ce19f6ed51be3c651ab72
abdoulDaff <abdoulniallydaff@gmail.com>
2022-09-22 03:29:26 +0000

    update the update method on our ChirpController class to validate the request and update the database.

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -84,6 +84,15 @@ class ChirpController extends Controller
     public function update(Request $request, Chirp $chirp)
     {
         //
+        $this->authorize('update', $chirp);
+ 
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $chirp->update($validated);
+ 
+        return redirect(route('chirps.index'));
     }
 
     /**
